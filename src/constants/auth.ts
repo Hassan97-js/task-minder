edad01c99b55876063b89ca65b92ed1e9b693adb
Hassan97-js <hassan.sweden.dev@gmail.com>
2024-02-20 19:37:50 +0100

    Add Next Auth Prisma Adapter

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -2,6 +2,10 @@ import { type AuthOptions } from "next-auth";
 import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "@/utils/db";
+import { Adapter } from "next-auth/adapters";
+
 const googleID = process.env.AUTH_GOOGLE_ID!;
 const googleSecret = process.env.AUTH_GOOGLE_SECRET!;
 
@@ -19,5 +23,6 @@ const githubConfig = {
 };
 
 export const authOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
   providers: [Google(googleConfig), Github(githubConfig)]
 } satisfies AuthOptions;
---
+++ src/utils/db.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+import { Pool } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
+
+function createPrisma() {
+  const connectionString = `${process.env.DATABASE_URL}`;
+  const pool = new Pool({ connectionString });
+  const adapter = new PrismaNeon(pool);
+
+  return new PrismaClient({ adapter });
+}
+
+declare global {
+  var prisma: undefined | ReturnType<typeof createPrisma>;
+}
+
+const prisma = globalThis.prisma ?? createPrisma();
+
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = prisma;
+}
+
+export default prisma;
